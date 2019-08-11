812e17843b476c5970c9a9e992d4e67ceada9e44
Campbell,Ian <icampbell9615@gmail.com>
2019-08-11 14:05:14 -0500

    Commit Testing

---
+++ public/js/budgetapp.js
@@ -0,0 +1,188 @@
+
+      FusionCharts.ready(function() {
+        var cSatScoreChart4 = new FusionCharts({
+    type: 'angulargauge',
+    renderAt: 'chart-container4',
+    width: '400',
+    height: '300',
+    dataFormat: 'json',
+    dataSource: {
+      "chart": {
+        "caption": "Entertainment",
+        "subcaption": "Last week",
+        "lowerLimit": "0",
+        "upperLimit": "100",
+        "showValue": "1",
+        "valueBelowPivot": "1",
+        "majorTMNumber": "9",
+        "minorTMNumber": "4",
+        "gaugeFillMix": "{dark-40},{light-40},{dark-20}",
+        "theme": "fusion"
+      },
+      "colorRange": {
+        "color": [{
+            "minValue": "0",
+            "maxValue": "50",
+            "code": "#6baa01"
+          },
+          {
+            "minValue": "50",
+            "maxValue": "75",
+            "code": "#f8bd19"
+          },
+          {
+            "minValue": "75",
+            "maxValue": "100",
+            "code": "#e44a00"
+          }
+        ]
+      },
+      "dials": {
+        "dial": [{
+          "value": "300"
+        }]
+      }
+    }
+  }).render();
+
+
+
+        var cSatScoreChart3 = new FusionCharts({
+    type: 'angulargauge',
+    renderAt: 'chart-container3',
+    width: '400',
+    height: '300',
+    dataFormat: 'json',
+    dataSource: {
+      "chart": {
+        "caption": "Transportation",
+        "subcaption": "Last week",
+        "lowerLimit": "0",
+        "upperLimit": "100",
+        "showValue": "1",
+        "valueBelowPivot": "1",
+        "majorTMNumber": "9",
+        "minorTMNumber": "4",
+        "gaugeFillMix": "{dark-40},{light-40},{dark-20}",
+        "theme": "fusion"
+      },
+      "colorRange": {
+        "color": [{
+            "minValue": "0",
+            "maxValue": "50",
+            "code": "#6baa01"
+          },
+          {
+            "minValue": "50",
+            "maxValue": "75",
+            "code": "#f8bd19"
+          },
+          {
+            "minValue": "75",
+            "maxValue": "100",
+            "code": "#e44a00"
+          }
+        ]
+      },
+      "dials": {
+        "dial": [{
+          "value": "67"
+        }]
+      }
+    }
+  }).render();
+
+
+        var cSatScoreChart2 = new FusionCharts({
+    type: 'angulargauge',
+    renderAt: 'chart-container2',
+    width: '400',
+    height: '300',
+    dataFormat: 'json',
+    dataSource: {
+      "chart": {
+        "caption": "Housing",
+        "subcaption": "Last week",
+        "lowerLimit": "0",
+        "upperLimit": "100",
+        "showValue": "1",
+        "valueBelowPivot": "1",
+        "majorTMNumber": "9",
+        "minorTMNumber": "4",
+        "gaugeFillMix": "{dark-40},{light-40},{dark-20}",
+        "theme": "fusion"
+      },
+      "colorRange": {
+        "color": [{
+            "minValue": "0",
+            "maxValue": "50",
+            "code": "#6baa01"
+          },
+          {
+            "minValue": "50",
+            "maxValue": "75",
+            "code": "#f8bd19"
+          },
+          {
+            "minValue": "75",
+            "maxValue": "100",
+            "code": "#e44a00"
+          }
+        ]
+      },
+      "dials": {
+        "dial": [{
+          "value": "67"
+        }]
+      }
+    }
+  }).render();
+
+  var cSatScoreChart = new FusionCharts({
+    type: 'angulargauge',
+    renderAt: 'chart-container',
+    width: '400',
+    height: '300',
+    dataFormat: 'json',
+    dataSource: {
+      "chart": {
+        "caption": "Food",
+        "subcaption": "Last week",
+        "lowerLimit": "0",
+        "upperLimit": "100",
+        "showValue": "1",
+        "valueBelowPivot": "1",
+        "majorTMNumber": "9",
+        "minorTMNumber": "4",
+        "gaugeFillMix": "{dark-40},{light-40},{dark-20}",
+        "theme": "fusion"
+      },
+      "colorRange": {
+        "color": [{
+            "minValue": "0",
+            "maxValue": "50",
+            "code": "#6baa01"
+          },
+          {
+            "minValue": "50",
+            "maxValue": "75",
+            "code": "#f8bd19"
+          },
+          {
+            "minValue": "75",
+            "maxValue": "100",
+            "code": "#e44a00"
+          }
+        ]
+      },
+      "dials": {
+        "dial": [{
+          "value": "67"
+        }]
+      }
+    }
+  }).render();
+
+       
+
+      });
