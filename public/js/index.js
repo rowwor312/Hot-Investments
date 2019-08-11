be7bdd26c9079b9d8b4e39d521ee1dee56b294ec
Enragedtoast <edwardsjtimothy@gmail.com>
2019-08-10 19:55:09 -0500

    added comments to index.js

--- public/js/index.js
+++ public/js/index.js
@@ -1,16 +1,18 @@
 $(function() {
-  console.log("ready!");
-
+  //click function for create account button to add extra field for Income
   $("#new-acc-btn").click(function(event) {
     event.preventDefault();
 
+    //creating new container div and giving it the "form-group" class
     var newForm = $("<div>");
     newForm.addClass("form-group");
 
+    //creating new lable element and adding the "for" attribute and "Income" heading
     var newLabel = $("<label>");
     newLabel.attr("for", "Income");
     newLabel.text("Income");
 
+    //creating new input element and adding attributes
     var newInput = $("<input>");
     newInput.attr({
       class: "form-control",
@@ -19,15 +21,21 @@ $(function() {
       placeholder: "10000000"
     });
 
+    //prepending new label and input to the newForm div
     newForm.prepend(newInput);
     newForm.prepend(newLabel);
 
+    //appending newForm div to the wider container div
     $(".input-con").append(newForm);
     $(".underline").text("Account Creation");
 
+    //hiding create account button
     $("#new-acc-btn").hide();
-    $("#login").text("Submit")
+    //changing text of login button to "Submit"
+    $("#login").text("Submit");
+    //changing login button color to yellow w/ bootstrap class
     $("#login").attr("class", "btn-warning");
+    //adding bootstrap btn class for style
     $("#login").addClass("btn");
   });
 });
