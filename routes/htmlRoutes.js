d4444d639bea4330d3b2a4d9a6357dee6b3a79ff
BigECire <ericsanders5800@gmail.com>
2019-08-16 11:34:46 -0500

    htmlroutes work

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,20 +1,20 @@
 var db = require("../models");
+var path = require("path");
 
 module.exports = function(app) {
   // Load index page
   app.get("/", function(req, res) {
-    res.render("index", {
-      msg: "Welcome!"
-    });
+    res.sendFile(path.join(__dirname, "../public/index.html"));
   });
 
   // Load example page and pass in an example by id
   app.get("/category/:id", function(req, res) {
-    db.Category.findOne({
+    db.Category.findAll({
       where: {
-        id: req.params.id
+        Userid: req.params.id
       },
       attributes: ["category"],
+      group: ["category"],
       include: [
         {
           model: db.useExp,
@@ -29,14 +29,33 @@ module.exports = function(app) {
   });
 
   app.get("/users/:id", function(req, res) {
-    db.sequelize.query("SELECT `User`.`id`, `User`.`userName`, `User`.`totalBudget`, `Categories->useExp`.`id` AS `Categories.useExp.id`, sum(`spent`) AS `Categories.useExp.total_spent` FROM `Users` AS `User` LEFT OUTER JOIN `Categories` AS `Categories` ON `User`.`id` = `Categories`.`UserId` LEFT OUTER JOIN `useExp` AS `Categories->useExp` ON `Categories`.`id` = `Categories->useExp`.`CategoryId` WHERE `User`.`id` = '1'",{ type: db.Sequelize.QueryTypes.SELECT }).then(function(data){
-      console.log(data)
-      res.json(data)
-    })
+    db.sequelize
+      .query(
+        "SELECT `User`.`id`, `User`.`userName`, `User`.`totalBudget`, `Categories->useExp`.`id` AS `Categories.useExp.id`, sum(`spent`) AS `Categories.useExp.total_spent` FROM `Users` AS `User` LEFT OUTER JOIN `Categories` AS `Categories` ON `User`.`id` = `Categories`.`UserId` LEFT OUTER JOIN `useExps` AS `Categories->useExp` ON `Categories`.`id` = `Categories->useExp`.`CategoryId` WHERE `User`.`id` = '1'", 
+        { type: db.Sequelize.QueryTypes.SELECT })
+      .then(function(data) {
+         db.Category.findAll({
+          where: {
+            Userid: req.params.id
+          },
+          attributes: ["category"],
+          group: ["category"],
+          include: [
+            {
+              model: db.useExp,
+              attributes: [
+                [db.sequelize.fn("sum", db.sequelize.col("spent")), "total_spent"]
+              ]
+            }
+          ]
+        }).then(function(dbCat) {
+          res.json({ top: data, gague: dbCat });
+        });
+      })
   });
 
   // Render 404 page for any unmatched routes
-  app.get("*", function (req, res) {
+  app.get("*", function(req, res) {
     res.render("404");
   });
 };
